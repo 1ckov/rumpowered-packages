e04f3c137be3b0cd269cf30fc302c922f00b0469
hackernoodle <silentnoodle@fedora.email>
2023-06-12 19:39:59 +0300

    update workflow

--- aursync.sh
+++ aursync.sh
@@ -1,2 +1,2 @@
 #!/bin/env bash
-aur sync --ignore-file=aur.ignore -u --pacman-conf "$PWD/pacman.conf" --makepkg-conf "$PWD/makepkg.conf" --remove --noview "$@"
+aur sync --ignore-file=aur.ignore -u --pacman-conf "$PWD/pacman.conf" --makepkg-conf "$PWD/makepkg.conf" --remove --noview --noconfirm "$@"
